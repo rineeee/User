1bc124cb31c887c42e0d60772c667e44313af8b4
rineeee <harin14@naver.com>
2023-08-22 20:47:05 +0900

    feat: add repository to infra

--- src/main/java/com/example/userservice/infrastructure/db/jpa/UserRepository.java
+++ src/main/java/com/example/userservice/infrastructure/db/jpa/UserRepository.java
@@ -1,5 +1,6 @@
-package com.example.userservice.repository;
+package com.example.userservice.infrastructure.db.jpa;
 
+import com.example.userservice.domain.entity.UserEntity;
 import org.springframework.data.repository.CrudRepository;
 
 public interface UserRepository extends CrudRepository<UserEntity, Long> {
